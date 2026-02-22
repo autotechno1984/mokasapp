abb8c4bf6624c3cd563213b9c826db7a48ae09f0
autotechno1984 <autotechno1984@gmail.com>
2026-02-22 16:26:01 +0700

    mokas-app

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
-    'domain' => env('APP_DOMAIN', 'mokasapp.test'),
+    'domain' => env('APP_DOMAIN', 'mokasapp.com'),
 
     /*
     |--------------------------------------------------------------------------
